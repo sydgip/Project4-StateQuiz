fb5b232aafbabc3f3da5cfad579d802e3c437058
sydgip <sydgipson@gmail.com>
2023-11-07 21:20:52 -0500

    Added insert methods

--- app/src/main/java/edu/uga/cs/project4/DBhelper.java
+++ app/src/main/java/edu/uga/cs/project4/DBhelper.java
@@ -3,12 +3,13 @@ package edu.uga.cs.project4;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBhelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "quiz_database";
-    private static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "quiz_database";
+    public static final int DATABASE_VERSION = 1;
 
     // Table names
     public static final String TABLE_QUESTIONS = "quiz_questions";
@@ -17,7 +18,7 @@ public class DBhelper extends SQLiteOpenHelper {
 
 
     // Table columns for quiz_questions
-    private static final String KEY_QUESTION_ID = "question_id";
+    public static final String KEY_QUESTION_ID = "question_id";
 
     public static final String KEY_STATE = "state";
     public static final String KEY_CAPITAL_CITY = "capital_city";
@@ -25,17 +26,17 @@ public class DBhelper extends SQLiteOpenHelper {
     public static final String KEY_ADDITIONAL_CITY3 = "additional_city2";
 
     // Table columns for quizzes
-    private static final String KEY_QUIZ_ID = "quiz_id";
-    private static final String KEY_QUIZ_DATE = "quiz_date";
-    private static final String KEY_QUIZ_RESULT = "quiz_result";
-    private static final String KEY_QUESTIONS_ANSWERED = "questions_answered";
+    public static final String KEY_QUIZ_ID = "quiz_id";
+    public static final String KEY_QUIZ_DATE = "quiz_date";
+    public static final String KEY_QUIZ_RESULT = "quiz_result";
+    public static final String KEY_QUESTIONS_ANSWERED = "questions_answered";
 
     // Table columns for quiz_question_relationship
-    private static final String KEY_USER_ANSWER = "user_answer";
+    public static final String KEY_USER_ANSWER = "user_answer";
 
     // Table columns for user_scores
-    private static final String KEY_USER_ID = "user_id";
-    private static final String KEY_USER_SCORE = "user_score";
+    public static final String KEY_USER_ID = "user_id";
+    public static final String KEY_USER_SCORE = "user_score";
 
     // This table holds the information read in from the CSV .
     private static final String CREATE_TABLE_QUESTIONS = "CREATE TABLE " + TABLE_QUESTIONS + "(" +
@@ -56,13 +57,61 @@ public class DBhelper extends SQLiteOpenHelper {
             KEY_QUIZ_ID + " INTEGER," +
             KEY_QUESTION_ID + " INTEGER," +
             KEY_USER_ANSWER + " TEXT," +
-            KEY_CAPITAL_CITY + "TEXT," +
             "PRIMARY KEY (" + KEY_QUIZ_ID + "," + KEY_QUESTION_ID + ")," +
             "FOREIGN KEY (" + KEY_QUIZ_ID + ") REFERENCES " + TABLE_SAVE_QUIZZES + "(" + KEY_QUIZ_ID + ")," +
             "FOREIGN KEY (" + KEY_QUESTION_ID + ") REFERENCES " + TABLE_QUESTIONS + "(" + KEY_QUESTION_ID + ")" +
-            "FOREIGN KEY (" + KEY_CAPITAL_CITY + ") REFERENCES " + TABLE_QUESTIONS + "(" + KEY_CAPITAL_CITY + ")" +
             ")";
 
+    public long getExistingQuizId() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        long quizId = -1; // Default value in case no ID is found
+
+        String query = "SELECT " + KEY_QUIZ_ID + " FROM " + TABLE_SAVE_QUIZZES + " LIMIT 1"; // Change the table name and column name as per your actual schema
+
+        Cursor cursor = db.rawQuery(query, null);
+        if (cursor.moveToFirst()) {
+            quizId = cursor.getLong(cursor.getColumnIndex(KEY_QUIZ_ID));
+        }
+
+        cursor.close();
+        db.close();
+
+        return quizId;
+    }
+
+    public long getExistingQuestionId() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        long questionId = -1; // Default value in case no ID is found
+
+        String query = "SELECT " + KEY_QUESTION_ID + " FROM " + TABLE_QUESTIONS + " LIMIT 1"; // Change the table name and column name as per your actual schema
+
+        Cursor cursor = db.rawQuery(query, null);
+        if (cursor.moveToFirst()) {
+            questionId = cursor.getLong(cursor.getColumnIndex(KEY_QUESTION_ID));
+        }
+
+        cursor.close();
+        db.close();
+
+        return questionId;
+    }
+
+
+
+
+
+
+    public void insertRelationship(int quizId, int questionId, String userAnswer) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_QUIZ_ID, quizId);
+        values.put(KEY_QUESTION_ID, questionId);
+        values.put(KEY_USER_ANSWER, userAnswer);
+
+        // Insert the row
+        db.insert(TABLE_RELATIONSHIP, null, values);
+        db.close(); // Closing database connection
+    }
 
     public DBhelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -96,4 +145,31 @@ public class DBhelper extends SQLiteOpenHelper {
         db.insert(TABLE_QUESTIONS, null, values);
         db.close(); // Closing database connection
     }
+    public void insertQuizResult(String date, int result, int questionsAnswered) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_QUIZ_DATE, date);
+        values.put(KEY_QUIZ_RESULT, result);
+        values.put(KEY_QUESTIONS_ANSWERED, questionsAnswered);
+
+        // Insert the row
+        db.insert(TABLE_SAVE_QUIZZES, null, values);
+        db.close(); // Closing database connection
+    }
+
+    public String getCorrectAnswerForQuestion(int questionId) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = {KEY_CAPITAL_CITY}; // Assuming KEY_CAPITAL_CITY is the correct answer column in TABLE_QUESTIONS
+        String selection = KEY_QUESTION_ID + " = ?";
+        String[] selectionArgs = {String.valueOf(questionId)};
+        Cursor cursor = db.query(TABLE_QUESTIONS, columns, selection, selectionArgs, null, null, null);
+        String correctAnswer = null;
+        if (cursor.moveToFirst()) {
+            correctAnswer = cursor.getString(cursor.getColumnIndex(KEY_CAPITAL_CITY));
+        }
+        cursor.close();
+        db.close();
+        return correctAnswer;
+    }
+
 }
